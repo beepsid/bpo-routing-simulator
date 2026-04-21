c9afe9eeba5834f4f47fd99048867e6bc196d47e
ΛYUSH <siddhantsinghchauhan1@gmail.com>
2026-04-21 22:07:41 +0530

    Create db and table

---
+++ sql/create_tables.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE bpo_project;
+USE bpo_project;
+
+CREATE TABLE call_center_logs (
+    call_id CHAR(6) PRIMARY KEY,
+    call_timestamp DATETIME NOT NULL,
+    agent_id CHAR(4) NOT NULL,
+    issue_category VARCHAR(20),
+    call_duration_seconds INT,
+    queue_wait_time INT,
+    resolution_status VARCHAR(20)
+);
